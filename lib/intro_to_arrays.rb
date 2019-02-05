42efa5efcfdb10f14ebfb133752707aea5fd2bb8
Olivia Auzenne <oauzenne@gmail.com>
2019-02-05 18:41:06 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,28 @@
+def instantiate_new_array
+  @my_new_array = []
+end
+
+def array_with_two_elements
+  @my_two_array = ["dog", "cat"]
+end
+  
+def first_element(array)
+   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+   @taylor_swift[0]
+end
+
+def third_element(array)
+   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+   @taylor_swift[2]
+end
+
+def last_element(array)
+   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+   @taylor_swift[-1]
+end
+
+def first_element_with_array_methods
+  @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  @south_east_asia.first
+end
+  
