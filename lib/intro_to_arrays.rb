dc767dd19a99f2178d43c5918f5f063538b0ee8c
Olivia Auzenne <oauzenne@gmail.com>
2019-02-05 18:47:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -31,7 +31,8 @@ def last_element_with_array_methods(array)
   @south_east_asia.last
 end
 
-def length_of_array
+def length_of_array(array)
   @programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
-  @programming_languages
+  @programming_languages.count
+end
   
